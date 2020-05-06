2f6327deeef6d52bdaea142bbaa41e77133f76aa
nishio1993 <grace240@docomo.ne.jp>
2020-05-06 21:53:44 +0900

    matchを用いたFizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,12 @@ fn main() {
 }
 
 fn fizzbuzz() {
-    /*
-    * for a in 1 .. 101で1以上101未満ループ
-    * for a in 1 ..=100で1以上100以下ループ
-    * for a in (1 .. 101).rev()で逆順
-    * for a in b.iter()で配列のループ
-    */
     for x in 1 .. 101 {
-        if x % 15 == 0 {
-            println!("FizzBuzz");
-        } else if x % 3 == 0 {
-            println!("Fizz");
-        } else if x % 5 == 0 {
-            println!("Buzz");
-        } else {
-            println!("{}", x);
+        match x % 15 {
+            0 => println!("FizzBuzz"),
+            3 | 6 | 9 | 12 => println!("Fizz"), // 「|」で区切ると複数指定
+            5 | 10 => println!("Buzz"),
+            _ => println!("{}", x)  //「_」でワイルドカード
         }
     }
 }
