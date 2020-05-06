ecd1d823781d2d03ed9e161b3d3c9ba38a5fabbf
nishio1993 <grace240@docomo.ne.jp>
2020-05-07 01:12:35 +0900

    matchを式として用いて、printlnマクロを呼び出す回数を減らす

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ fn main() {
 
 fn fizzbuzz() {
     for x in 1 .. 101 {
-        match (x % 3, x % 5) {
-            (0, 0) => println!("FizzBuzz"),
-            (0, _) => println!("Fizz"),
-            (_, 0) => println!("Buzz"),
-            (_, _) => println!("{}", x)
-        }
+        let s:String = match (x % 3, x % 5) {
+            (0, 0) => "FizzBuzz".to_string(),   //文字列リテラルは「文字列スライス」という型
+            (0, _) => "Fizz".to_string(),       //静的に領域が確保されるため、Stringとは違う型になる
+            (_, 0) => "Buzz".to_string(),       //Stringは可変長文字列である
+            (_, _) => x.to_string()
+        };  //式なので「;」が必要
+        println!("{}", s);
     }
 }
