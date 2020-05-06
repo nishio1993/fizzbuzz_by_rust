5228ecc5aa4250419372793007ec030019abd2f5
nishio1993 <grace240@docomo.ne.jp>
2020-05-06 22:38:03 +0900

    パターンを用いて、「3|6|9|12」を「x % 3 == 0」に置換する。

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,11 @@ fn main() {
 
 fn fizzbuzz() {
     for x in 1 .. 101 {
-        match x % 15 {
-            0 => println!("FizzBuzz"),
-            3 | 6 | 9 | 12 => println!("Fizz"), // 「|」で区切ると複数指定
-            5 | 10 => println!("Buzz"),
-            _ => println!("{}", x)  //「_」でワイルドカード
+        match x {
+            e if e % 15 == 0 => println!("FizzBuzz"),
+            e if e % 3 == 0  => println!("Fizz"),
+            e if e % 5 == 0  => println!("Buzz"),
+            e => println!("{}", e)
         }
     }
 }
