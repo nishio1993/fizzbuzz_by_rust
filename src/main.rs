dfd688f58ea27fe8d412281202a49406bfe646e6
nishio1993 <grace240@docomo.ne.jp>
2020-05-06 18:54:42 +0900

    Forを用いたFizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,13 @@ fn main() {
 }
 
 fn fizzbuzz() {
-    let mut x:i32 = 0;
-
-    while x <= 100 {
+    /*
+    * for a in 1 .. 101で1以上101未満ループ
+    * for a in 1 ..=100で1以上100以下ループ
+    * for a in (1 .. 101).rev()で逆順
+    * for a in b.iter()で配列のループ
+    */
+    for x in 1 .. 101 {
         if x % 15 == 0 {
             println!("FizzBuzz");
         } else if x % 3 == 0 {
@@ -15,7 +19,5 @@ fn fizzbuzz() {
         } else {
             println!("{}", x);
         }
-
-        x += 1;
     }
 }
