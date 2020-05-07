b2d1535ad781a9b71075d2502564c30e816cd548
nishio1993 <grace240@docomo.ne.jp>
2020-05-07 20:34:53 +0900

    クロージャを用いてFizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,17 @@ fn main() {
 }
 
 fn fizzbuzz() {
-    for x in 1 .. 101 {
-        let tmp:String; //s:&str = &x.to_string();の場合、sの参照先はmatch式ブロックの中にあり、値が破棄されているためコンパイル不可能
-        let s:&str = match (x % 3, x % 5) {
-            (0, 0) => "FizzBuzz",
-            (0, _) => "Fizz",
-            (_, 0) => "Buzz",
-            (_, _) => {tmp = x.to_string(); &tmp}   //ブロック外の変数に値を入れることで、参照先がライフタイムを終えていないことを保証する
-        };
-        println!("{}", s);
-    }
+    let fizzbuzz = |x: i32| -> String {     //クロージャの文法は|変数:型| -> 返り値型 {}
+        match (x % 3, x % 5) {
+            (0, 0) => format!("FizzBuzz"),  //format!の返り値はstd::string::String
+            (0, _) => format!("Fizz"),      //format!無しの文字列リテラルは&str
+            (_, 0) => format!("Buzz"),
+            (_, _) => x.to_string()
+        }
+    };  //クロージャ定義は関数定義と違い、末尾に「;」が必要
+    let println = |x| { //クロージャの変数、返り値は型省略可能
+        println!("{}", x);
+    };
+
+    (1 ..= 100).map(fizzbuzz).for_each(println);
 }
