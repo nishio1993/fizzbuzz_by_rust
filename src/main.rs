95d33097450132028a5b4bb0f6992d3db0d33e16
nishio1993 <grace240@docomo.ne.jp>
2020-05-07 16:01:39 +0900

    to_string()の使用回数を少なくすることで、ヒープの使用量を減らす。

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ fn main() {
 
 fn fizzbuzz() {
     for x in 1 .. 101 {
-        let s:String = match (x % 3, x % 5) {
-            (0, 0) => "FizzBuzz".to_string(),   //文字列リテラルは「文字列スライス」という型
-            (0, _) => "Fizz".to_string(),       //静的に領域が確保されるため、Stringとは違う型になる
-            (_, 0) => "Buzz".to_string(),       //Stringは可変長文字列である
-            (_, _) => x.to_string()
-        };  //式なので「;」が必要
+        let tmp:String; //s:&str = &x.to_string();の場合、sの参照先はmatch式ブロックの中にあり、値が破棄されているためコンパイル不可能
+        let s:&str = match (x % 3, x % 5) {
+            (0, 0) => "FizzBuzz",
+            (0, _) => "Fizz",
+            (_, 0) => "Buzz",
+            (_, _) => {tmp = x.to_string(); &tmp}   //ブロック外の変数に値を入れることで、参照先がライフタイムを終えていないことを保証する
+        };
         println!("{}", s);
     }
 }
